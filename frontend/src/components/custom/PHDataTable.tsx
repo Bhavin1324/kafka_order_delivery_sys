1c311b959c3256b06863fdfa6626826b5798ff70
bhavin1324 <bjariwala26@gmail.com>
2023-12-13 21:20:52 +0530

    ignoration modification

--- frontend/src/components/custom/PHDataTable.tsx
+++ frontend/src/components/custom/PHDataTable.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { DeliveryPerson, Item, Outlet } from "../../types/interfaces";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import ErrorOutlineOutlinedIcon from "@mui/icons-material/ErrorOutlineOutlined";
+import { nanoid } from "@reduxjs/toolkit";
 interface TableProps<T> {
   data: T[];
   title: string;
@@ -11,7 +13,7 @@ interface TableProps<T> {
 }
 type T = Outlet | Item | DeliveryPerson;
 function PHDataTable(props: TableProps<T>) {
-  const [list, setList] = useState(props.data);
+  const [list, setList] = useState(props.data.length ? props.data : []);
   const [search, setSearch] = useState("");
 
   const renderList = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -29,8 +31,21 @@ function PHDataTable(props: TableProps<T>) {
     } else {
       setList(newList);
     }
+    console.log(list);
   };
-  useEffect(() => {}, [props]);
+  useEffect(() => {
+    console.log(props.data.length);
+    if (
+      props.data == null ||
+      props.data == undefined ||
+      props.data.length == 0
+    ) {
+      setList([]);
+    } else {
+      setList(props.data);
+    }
+  }, [props]);
+
   return (
     <div className="mt-4 overflow-x-auto">
       <div className="container">
@@ -44,48 +59,61 @@ function PHDataTable(props: TableProps<T>) {
             value={search}
           />
         </div>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              {Object.keys(list[0]).map((item) => {
-                return <th scope="col">{item}</th>;
-              })}
-              <td></td>
-              <td></td>
-            </tr>
-          </thead>
-          <tbody>
-            {list.map((item) => {
-              return (
-                <tr>
-                  {Object.keys(item).map((x) => {
-                    return <td>{item[x]}</td>;
+        {props.data.length == 0 && (
+          <div className="text-secondary flex justify-center my-2">
+            <ErrorOutlineOutlinedIcon />
+            <span className="ms-2">No record found</span>
+          </div>
+        )}
+        {props.data.length > 0 && (
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                {list.length > 0 &&
+                  Object.keys(list[0]).map((item) => {
+                    return (
+                      <th key={nanoid()} scope="col">
+                        {item}
+                      </th>
+                    );
                   })}
-                  <td>
-                    <button
-                      className="btn btn-primary"
-                      onClick={() => {
-                        props.onUpadate(item);
-                      }}
-                    >
-                      <EditIcon />
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => {
-                        props.onDelete(item?.id);
-                      }}
-                    >
-                      <DeleteIcon />
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                <td></td>
+                <td></td>
+              </tr>
+            </thead>
+            <tbody>
+              {list.map((item) => {
+                return (
+                  <tr key={nanoid()}>
+                    {Object.keys(item).map((x) => {
+                      return <td key={nanoid()}>{item[x]}</td>;
+                    })}
+                    <td>
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => {
+                          props.onUpadate(item);
+                        }}
+                      >
+                        <EditIcon />
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => {
+                          props.onDelete(item?.id);
+                        }}
+                      >
+                        <DeleteIcon />
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
--- frontend/src/types/commons.ts
+++ frontend/src/types/commons.ts
@@ -1,2 +1,2 @@
 export type HttpMethods = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
-export type Roles = "CUSTOMER" | "DELIVERY_PERSON" | "OUTLET" | "ADMIN";
+export type Roles = "Customer" | "DeliveryPerson" | "Admin" | "Outlet";
